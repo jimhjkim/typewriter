5ff3770d23a03b94484519caaf8d5473a7dc2f8c
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-09-24 23:31:32 +0000

    added another way to add new line after printing out the letters @haphamo @peterhangg

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,22 @@
 const sentence = "hello there from lighthouse labs";
 let wait = 0;
+const delay = 50;
+for (let char of sentence) {
+  if (wait !== (sentence.length - 1) * delay) {
+    setTimeout(() => { process.stdout.write(char) }, wait);
+    wait += delay;
+  } else {
+    setTimeout(() => { console.log(char) }, wait);
+  }
+};
 
-let sentenceArray = sentence.split('');
-sentenceArray.forEach((num, i, arr) => {
-  setTimeout(() => {
-    process.stdout.write(num)
-    if (arr.length - 1 - i === 0) {
-      console.log();
-    }
-  }, wait);
-  wait += 50;
-});
+// let sentenceArray = sentence.split('');
+// sentenceArray.forEach((num, i, arr) => {
+//   setTimeout(() => {
+//     process.stdout.write(num)
+//     if (arr.length - 1 - i === 0) {
+//       console.log();
+//     }
+//   }, wait);
+//   wait += 50;
+// });
